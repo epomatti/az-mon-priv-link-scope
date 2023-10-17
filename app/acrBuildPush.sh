eadd0ceb18472c34086ef67b94b5cbd8a76db32d
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-10-17 00:20:45 -0300

    docker

--- app/acrBuildPush.sh
+++ app/acrBuildPush.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 localTag="javaapp-local"
-acr=acrepicservicex
+acr=acrexamplefactory
 image="$acr.azurecr.io/javaapp:latest"
 
 docker build -t $localTag .
-az acr login --name $acrName
+az acr login --name $acr
 docker tag $localTag $image
 docker push $image
